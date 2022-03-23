0146ddf4cc623018535008a54338fc215e762cdd
kevinsuttits21 <93242927+kevinsuttits21@users.noreply.github.com>
2022-03-23 13:50:08 +0200

    Lisafunktsioonid ehk koodi täiendus

--- kv.py
+++ kv.py
@@ -1,40 +1,56 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
+from urllib.request import urlopen
+import re
 import ssl
 from fake_useragent import UserAgent
 ssl._create_default_https_context = ssl._create_unverified_context
-defaultURL = 'https://www.kv.ee/?act=search.simple&company_id=237&deal_type=1&company_id_check=237&county=1&search_type=new&parish=1061&city%5B0%5D=1007'
+# selenium
 chromeDriver = 'C:/Users/kevin.sutt/Downloads/selenium/chromedriver.exe'
 
-def startSelenium():
+def startSelenium(url):
     optionss = webdriver.ChromeOptions()
     optionss.add_argument('--disable-blink-features=AutomationControlled')
     optionss.add_experimental_option("excludeSwitches", ["enable-automation"])
     optionss.add_experimental_option('useAutomationExtension', False)
-    optionss.add_argument("window-size=1600,1200")
+    optionss.add_argument("window-size=1920,1080")
     ua = UserAgent()
     user_agent = ua.random
     print(user_agent)
     optionss.add_argument(f'user-agent={user_agent}')
     browser = webdriver.Chrome(options=optionss, executable_path=chromeDriver)
     browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-    browser.get(defaultURL)
+    browser.get(url)
     return browser
-
-driver = startSelenium()
-parsedURL = defaultURL
-driver.get(parsedURL)
+a = []
+url = "https://www.kv.ee/?act=search.simple&company_id=237&deal_type=1&company_id_check=237&county=1&search_type=new&parish=1061&city%5B0%5D=1007"
+driver = startSelenium(url)
 source = driver.page_source
+# näide praegu
+# saab asendada neid kohti inputiga hiljem
+# regex nimed
+nimed = re.findall('((Tallinn, Mustamäe)(.*)( ))', source)
+#print(nimed)
+for nimi in nimed:
+    a.append(nimi[1])
 
-#headers = {'User-Agent': 'Mozilla/5.0'}
+# regex lingid
+lingid = re.findall('((https://)(.*)(html))', source)
+#print(lingid)
 
-#r = requests.get(url, headers=headers)
+# bsoup    
+doc = BeautifulSoup(source, "html.parser")
+# hinna leidmine
+hind = doc.find_all("p", {"class": "object-price-value"})
+a = re.findall(r'[\d ]+', str(hind))
+#print(a)
+t = "".join(a)
+a = re.sub("( ){2,}", " ", t)
+print(a.split(" "))
 
-#doc = BeautifulSoup(r.text, "html.parser")
+    
 
-#kv = doc.find_all("tbody")
 
-#tags = doc.find_all('a', class_="nav-title")
-#links = [url + tag["href"] for tag in tags]
+        
 
