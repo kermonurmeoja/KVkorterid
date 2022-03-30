af284b65c45173e7b7ac36d9524a2f9395f74960
kevinsuttits21 <93242927+kevinsuttits21@users.noreply.github.com>
2022-03-30 14:47:22 +0300

    Added user interface

--- kv.py
+++ kv.py
@@ -10,7 +10,6 @@ import getpass
 # selenium
 chromeDriver = 'C:/Users/' + getpass.getuser() + '/Downloads/chromedriver.exe'
 
-
 def startSelenium(url):
     optionss = webdriver.ChromeOptions()
     optionss.add_argument('--disable-blink-features=AutomationControlled')
@@ -25,10 +24,8 @@ def startSelenium(url):
     browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     browser.get(url)
     return browser
-
-
-a = []
-url = input("Sisestage url: ")
+kvee = []
+url = input("Sisestage KV.ee portaali URL: ")
 driver = startSelenium(url)
 source = driver.page_source
 k = []
@@ -50,41 +47,90 @@ a = re.findall(r'[\d ]+', str(hind))
 t = "".join(a)
 a = re.sub("( ){2,}", " ", t)
 hinnad = a.split(" ")
-#print(hinnad)
 kinnisvara = []
 a = 0
 b = 1
 d = 0
 e = 0
-print("-------------------------------------")
-print("Praegune kinnisvara info")
-print("-------------------------------------")
 while a < len(k):
     nimi = k[a].replace("  ", "")
     kinnisvara.append([k[a], hinnad[b], k2[a]])
-    print(k[a].replace("  ", "") + " | Hind: " + hinnad[b] + " € | Link: " + k2[a])
     a += 1
     b += 1
+a = 0
+b = 1
 
-# Siin on SQL db kood
+# SQL andmebaas
 import sqlite3
 
-ühendus = sqlite3.connect('KVdb.db')
-c = ühendus.cursor()
-c.execute("SELECT * FROM Mustamäe")
-# 3 value
-#c.execute("INSERT INTO Mustamäe (Aadress, Hind, Link) VALUES ('x', 'x', 'x')")
 
-k2sk = "INSERT INTO Mustamäe (Aadress, Hind, Link) VALUES (?, ?, ?)"
-i = 0
-d = 0
-while i < len(kinnisvara):
-    c.execute(k2sk, (str(kinnisvara[d][0]), str(kinnisvara[d][1]), str(kinnisvara[d][2])))
-    d = d + 1
-    i = i + 1
+print("-------------------------------------")
+print("Mustamäe kinnisvara andmebaas")
+print("Saadaval olevad käsud:")
+print("-- praegu - näitab praeguseid kinnisvaraüksusi koos hinna ja lingiga.")
+print("-- kanna - kannab andmebaasi praegused kinnisvaraüksused koos hinna ja lingiga.")
+print("-- vaata - vaata kinnisvaraüksuse hinna arengut.")
+print("-- katkesta - katkestab ühenduse.")
+print("-------------------------------------")
+e = 0
+while e < 1:
+    cmd = input("$ ")
+    if cmd == "praegu":
+        o = 1
+        print("Kuvan praegused aktiivsed pakkumised:")
+        while a < len(k):
+            print(str(o) + ". " + k[a].replace("  ", "") + " | Hind: " + hinnad[b] + " € | Link: " + k2[a])
+            a += 1
+            b += 1
+            o += 1
+        o = 0
+        a = 0
+        b = 1
+        e += 1
+        input("Klikka jätkamiseks...")
+    elif cmd == "kanna":
+        print("Kannan andmed andmebaasi...")
+        ühendus = sqlite3.connect('KVdb.db')
+        c = ühendus.cursor()
+        c.execute("SELECT * FROM Mustamäe")
+        # c.execute("INSERT INTO Mustamäe (Aadress, Hind, Link) VALUES ('x', 'x', 'x')")
+        k2sk = "INSERT INTO Mustamäe (Aadress, Hind, Link) VALUES (?, ?, ?)"
+        i = 0
+        d = 0
+        while i < len(kinnisvara):
+            c.execute(k2sk, (str(kinnisvara[d][0]), str(kinnisvara[d][1]), str(kinnisvara[d][2])))
+            d += 1
+            i += 1
+        c.close()
+        ühendus.commit()
+        ühendus.close()
+        print("Andmed on kantud andmebaasi.")
+        e += 1
+        input("Klikka jätkamiseks...")
+    elif cmd == "vaata":
+        jrknr = int(input("Sisesta järjekorranumber, mille kinnisvarahindu soovid vaadata: "))- 1
+        jrknr2 = int(jrknr) + int(1)
+        selekteeritud = str(k[jrknr].replace("  ", ""))
+        ühendus = sqlite3.connect('KVdb.db')
+        c = ühendus.cursor()
+        c.execute("SELECT * FROM Mustamäe")
+        c.execute("SELECT * FROM Mustamäe WHERE Aadress = ?", (selekteeritud,))
 
-c.close()
+        z = 0
+        selekteeritud_yksused = c.fetchall()
+        while z < len(selekteeritud_yksused):
+            print(selekteeritud_yksused[z][0] + " | Hind: "+ selekteeritud_yksused[z][1] + " € | " + selekteeritud_yksused[z][2])
+            z += 1
 
-ühendus.commit()
-
-ühendus.close()
+        c.close()
+        ühendus.commit()
+        ühendus.close()
+        e += 1
+        input("Klikka jätkamiseks...")
+    elif cmd == "katkesta":
+        print("Katkestan ühenduse.")
+        exit()
+    else:
+        print("Sisestatud on kehtetu käsk. Katkestan ühenduse.")
+        exit()
+    e = 0
