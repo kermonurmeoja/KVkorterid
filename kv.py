f4be28faa5ed538fd8d5fb8dd1f19b039909c428
kevinsuttits21 <93242927+kevinsuttits21@users.noreply.github.com>
2022-03-30 13:19:06 +0300

    Improved the code

--- kv.py
+++ kv.py
@@ -28,22 +28,17 @@ def startSelenium(url):
 
 
 a = []
-url = "https://www.kv.ee/?act=search.simple&company_id=237&deal_type=1&company_id_check=237&county=1&search_type=new&parish=1061&city%5B0%5D=1007"
+url = input("Sisestage url: ")
 driver = startSelenium(url)
 source = driver.page_source
-# näide praegu
-# saab asendada neid kohti inputiga hiljem
-# regex nimed
-nimed = re.findall('((Tallinn, Mustamäe)(.*)( ))', source)
-for nimi in nimed:
-    a.append(nimi[1])
-# regex lingid
-lingid = re.findall('((https://)(.*)(html))', source)
-c = 0
-lingid2 = []
-while c < len(lingid):
-    lingid2.append(lingid[c][0])
-    c += 2
+k = []
+k2 = []
+doc = BeautifulSoup(source, "html.parser")
+# tiitel ja lingid
+objektid = doc.find_all("a", {"class": "object-title-a text-truncate"})
+for obj in objektid:
+    k.append(obj.text.strip())
+    k2.append(obj['href'])
 
 hinnad = []
 # bsoup
@@ -55,7 +50,7 @@ a = re.findall(r'[\d ]+', str(hind))
 t = "".join(a)
 a = re.sub("( ){2,}", " ", t)
 hinnad = a.split(" ")
-
+#print(hinnad)
 kinnisvara = []
 a = 0
 b = 1
@@ -64,14 +59,13 @@ e = 0
 print("-------------------------------------")
 print("Praegune kinnisvara info")
 print("-------------------------------------")
-while a < len(nimed):
-    nimi = nimed[a][0].replace("  ", "")
-    kinnisvara.append([nimi, hinnad[b], lingid2[a]])
-    # print(nimed[a][0].replace("  ", "") + " | Hind: " + hinnad[b] + " € | Link: " + lingid2[a])
+while a < len(k):
+    nimi = k[a].replace("  ", "")
+    kinnisvara.append([k[a], hinnad[b], k2[a]])
+    print(k[a].replace("  ", "") + " | Hind: " + hinnad[b] + " € | Link: " + k2[a])
     a += 1
     b += 1
 
-print(kinnisvara)
 # Siin on SQL db kood
 import sqlite3
 
@@ -79,7 +73,7 @@ import sqlite3
 c = ühendus.cursor()
 c.execute("SELECT * FROM Mustamäe")
 # 3 value
-# c.execute("INSERT INTO Mustamäe (Aadress, Hind, Link) VALUES ('x', 'x', 'x')")
+#c.execute("INSERT INTO Mustamäe (Aadress, Hind, Link) VALUES ('x', 'x', 'x')")
 
 k2sk = "INSERT INTO Mustamäe (Aadress, Hind, Link) VALUES (?, ?, ?)"
 i = 0
